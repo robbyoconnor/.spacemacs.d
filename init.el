779fd9247d34c6d7605d29652958213ebea16e9e
Robert O'Connor <rob@oconnor.ninja>
2016-07-06 23:25:56 -0400

    Remove doc-view hacks.

--- init.el
+++ init.el
@@ -355,11 +355,6 @@ layers configuration."
             (lambda ()
               (ibuffer-switch-to-saved-filter-groups "Default")))
   (add-hook 'ibuffer-mode-hook 'ibuffer-auto-mode)
-  (require 'doc-view)
-  (define-key doc-view-mode-map (kbd "j") 'doc-view-next-line-or-next-page)
-  (define-key doc-view-mode-map (kbd "k") 'doc-view-previous-line-or-previous-page)
-  (define-key doc-view-mode-map (kbd "h") 'image-backward-hscroll)
-  (define-key doc-view-mode-map (kbd "l") 'image-forward-hscroll)
   (add-hook 'emacs-lisp-mode-hook
             (lambda ()
               (push '("add-hook" . ?) prettify-symbols-alist)
