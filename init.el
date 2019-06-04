cd88372c0edafe95b915ec98433538e907dd69c9
Robert O'Connor <rob@oconnor.ninja>
2019-06-04 17:34:59 -0400

    use new inverted doge banner

--- init.el
+++ init.el
@@ -329,7 +329,7 @@ It should only modify the values of Spacemacs settings."
    ;; directory. A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
-   dotspacemacs-startup-banner 'doge
+   dotspacemacs-startup-banner 'doge-inverted
 
    ;; List of items to show in startup buffer or an association list of
    ;; the form `(list-type . list-size)`. If nil then it is disabled.
